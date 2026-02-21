0f54daf3f16c48ff0f09c9b50b551295313ebcd2
Isaac <isaacfunes@icloud.com>
2026-02-20 22:20:28 -0600

    Preparing header and need to import logos

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className="main-container inner">
+            <link href="/">
+                <image src="/assets/logo.svg" alt="CoinPulse logo"
+                       width={132} height={40} />
+            </link>
+        </div>
     )
 }
 export default Header
