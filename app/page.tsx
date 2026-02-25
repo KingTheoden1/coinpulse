ec90f7ab26d2ee1868674b4289d59b605a8131f0
Isaac <isaacfunes@icloud.com>
2026-02-24 18:47:23 -0600

    Tested Junie AI to test dummy data

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ const columns: DataTableColumn<TrendingCoin>[] = [
             const item = coin.item
 
             return (
-                <Link href={`/coins/${item.id}`}>
+                <Link href={`/coins/${item.id}`} className="flex items-center gap-2">
                     <Image src={item.large} alt={item.name} width={36}
-                           height={36}/>
+                           height={36} className="rounded-full" />
                     <p>{item.name}</p>
                 </Link>
             )
@@ -32,19 +32,71 @@ const columns: DataTableColumn<TrendingCoin>[] = [
             return (
                 <div className={cn('price-change', isTrendingUp ?
                 'text-green-500' : 'text-red-500')}>
-                    <p>
+                    <p className="flex items-center gap-1">
                         {isTrendingUp ? (
                             <TrendingUp width={16} height={16}/>
                         ) :
                             <TrendingDown width={16} height={16}/>
                         }
+                        {item.data.price_change_percentage_24h.usd.toFixed(2)}%
                     </p>
                 </div>
             )
         }
     },
     { header: 'Price', cellClassName: 'price-cell', cell: (coin) =>
-    coin.item.data.price },
+    `$${coin.item.data.price.toLocaleString()}` },
+]
+
+const dummyCoins: TrendingCoin[] = [
+    {
+        item: {
+            id: 'bitcoin',
+            name: 'Bitcoin',
+            symbol: 'BTC',
+            market_cap_rank: 1,
+            thumb: 'https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png',
+            large: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png',
+            data: {
+                price: 89000,
+                price_change_percentage_24h: {
+                    usd: 2.5
+                }
+            }
+        }
+    },
+    {
+        item: {
+            id: 'ethereum',
+            name: 'Ethereum',
+            symbol: 'ETH',
+            market_cap_rank: 2,
+            thumb: 'https://assets.coingecko.com/coins/images/279/thumb/ethereum.png',
+            large: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png',
+            data: {
+                price: 2600,
+                price_change_percentage_24h: {
+                    usd: -1.2
+                }
+            }
+        }
+    },
+    {
+        item: {
+            id: 'solana',
+            name: 'Solana',
+            symbol: 'SOL',
+            market_cap_rank: 5,
+            thumb: 'https://assets.coingecko.com/coins/images/4128/thumb/solana.png',
+            large: 'https://assets.coingecko.com/coins/images/4128/large/solana.png',
+            data: {
+                price: 145,
+                price_change_percentage_24h: {
+                    usd: 5.7
+                }
+            }
+        }
+    }
 ]
 
 const Page = () => {
@@ -61,8 +113,11 @@ const Page = () => {
             </div>
 
             <p>Trending Coins</p>
-            <DataTable data={[]} columns={
-                [{ header: 'Title'}, { header: 'Price'}]} />
+            <DataTable 
+                data={dummyCoins} 
+                columns={columns} 
+                rowKey={(coin) => coin.item.id}
+            />
         </section>
 
         <section className="w-full mt-7 space-y-4">
